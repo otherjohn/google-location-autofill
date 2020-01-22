2fcef41040fe99aad26221e59b8e0a176d6968f2
otherjohn <john.hamman@gmail.com>
2020-01-22 12:54:31 -0500

    added important comment
    Signed-off-by: otherjohn <john.hamman@gmail.com>

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,6 +13,7 @@ class ServiceProvider extends AddonServiceProvider
         LocationFieldtype::class,
     ];
 
+
     protected $scripts = [
         __DIR__.'/../dist/js/location.js',
     ];
@@ -23,7 +24,7 @@ class ServiceProvider extends AddonServiceProvider
 
         parent::boot();
         $url = 'https://maps.googleapis.com/maps/api/js?libraries=places&key=' . config('google-location-autofill.google_places_api_key');
-        Statamic::externalScript($url);
+        Statamic::externalScript($url); /* <-- Just because you can import external scripts, doesn't mean you go around using it for crap like jquery cdn and such! Thats what NPM is used for. https://github.com/statamic/cms/issues/961#issuecomment-558690407  */
         $this->publishes([
             __DIR__ . '/config/google-location-autofill.php' => config_path('google-location-autofill.php'),
         ]);
